a7022918259d2111d38a3ff456a0ac454fd77a4a
VictorAlexEtec2403427 <victor.gouveia@cps.sp.gov.br>
2026-06-15 17:28:00 -0300

    alterando scripts

--- script.bat
+++ script.bat
@@ -1,52 +1,70 @@
 @echo off
-:: =====================================================
-:: Script de Configuração: PHP + Git + Repositório
-:: =====================================================
+setlocal EnableDelayedExpansion
 
-:: Configurar o caminho do PHP (ALTERE ESTE CAMINHO)
-set CAMINHO_PHP=C:\wamp64\bin\php\php8.3.14
+:: ================= CONFIGURAÇÕES =================
+set CAMINHO_PHP=C:\wamp64\bin\php\php8.2.0
+set CAMINHO_MYSQL=C:\wamp64\bin\mysql\mysql8.0.35\bin
+set USUARIO_MYSQL=root
+set SENHA_MYSQL=
+set NOME_PROJETO=aula_1
+set NOME_BANCO=Aula1
+set URL_COMPOSER=https://getcomposer.org/download/2.9.5/composer.phar
+:: ===============================================
 
-:: Adiciona ao PATH do Sistema (/M significa Machine/Sistema)
-echo [1/4] Adicionando %CAMINHO_PHP% ao PATH...
-setx /M PATH "%PATH%;%CAMINHO_PHP%"
-if %errorlevel% neq 0 (
-    echo.
-    echo [ERRO] Falha ao atualizar o PATH. Execute como Administrador.
+:: 1. Adiciona PHP ao PATH do Sistema
+echo [1/6] Configurando PHP no PATH...
+setx /M PATH "%PATH%;%CAMINHO_PHP%" >nul 2>&1
+if %ERRORLEVEL% NEQ 0 echo Aviso: Pode ser necessario executar como Administrador.
+
+:: 2. Adiciona MySQL ao PATH temporariamente (para esta sessão)
+echo [2/6] Configurando MySQL...
+set PATH=%PATH%;%CAMINHO_MYSQL%
+
+:: 3. Baixa o Composer
+echo [3/6] Baixando composer.phar...
+powershell -Command "Invoke-WebRequest -Uri '%URL_COMPOSER%' -OutFile 'composer.phar'"
+if not exist composer.phar (
+    echo Erro: Nao foi possivel baixar o composer.phar
     pause
     exit /b 1
 )
 
-:: Configurações do Git (PERSONALIZE ESTES VALORES)
-echo.
-echo [2/4] Configurando Git...
-git config --global user.name "VictorAlexEtec2403427"
-git config --global user.email "victor.gouveia@aluno.cps.sp.gov.br"
-
-if %errorlevel% neq 0 (
-    echo.
-    echo [ERRO] Falha ao configurar o Git. Verifique se o Git esta instalado.
+:: 4. Cria o projeto Laravel
+echo [4/6] Criando projeto Laravel '%NOME_PROJETO%'...
+php composer.phar create-project laravel/laravel %NOME_PROJETO%
+if %ERRORLEVEL% NEQ 0 (
+    echo Erro na criacao do projeto Laravel
     pause
     exit /b 1
 )
 
-:: Clonar repositório (ALTERE O USUÁRIO E CAMINHO SE NECESSÁRIO)
-echo.
-echo [3/4] Clonando repositorio...
-git clone https://github.com/<usuariogit>/aula1-crud-laravel.git
-if %errorlevel% neq 0 (
-    echo.
-    echo [ERRO] Falha ao clonar o repositorio. Verifique a URL e sua conexao com a internet.
-    pause
-    exit /b 1
+:: 5. Abre a pasta do projeto
+echo [5/6] Abrindo pasta do projeto...
+start "" "%~dp0%NOME_PROJETO%"
+
+:: 6. Cria o banco de dados no MySQL
+echo [6/6] Criando banco de dados '%NOME_BANCO%'...
+if "%SENHA_MYSQL%"=="" (
+    mysql -u %USUARIO_MYSQL% -e "CREATE DATABASE IF NOT EXISTS %NOME_BANCO% CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+) else (
+    mysql -u %USUARIO_MYSQL% -p%SENHA_MYSQL% -e "CREATE DATABASE IF NOT EXISTS %NOME_BANCO% CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+)
+if %ERRORLEVEL% NEQ 0 (
+    echo Aviso: Nao foi possivel criar o banco de dados automaticamente.
+    echo Crie manualmente com: mysql -u %USUARIO_MYSQL% -p
+    echo E execute: CREATE DATABASE %NOME_BANCO%;
 )
 
-:: Conclusão
 echo.
-echo [4/4] Concluido!
+echo ================= CONCLUÍDO! =================
 echo.
-echo Para validar:
-echo   - Abra um NOVO CMD e digite: php -v
-echo   - Para verificar o Git: git --version
-echo   - O projeto foi clonado na pasta: aula1-crud-laravel
+echo Proximos passos:
+echo 1. Acesse a pasta: cd %NOME_PROJETO%
+echo 2. Configure o arquivo .env com os dados do banco:
+echo    DB_DATABASE=%NOME_BANCO%
+echo    DB_USERNAME=%USUARIO_MYSQL%
+echo    DB_PASSWORD=%SENHA_MYSQL%
+echo 3. Execute as migrations: php artisan migrate
+echo 4. Inicie o servidor: php artisan serve
 echo.
 pause
